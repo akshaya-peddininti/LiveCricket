83e29cb0dc9a47490919a1d3b4fa100259ae6369
akshaya-peddininti <146316779+akshaya-peddininti@users.noreply.github.com>
2024-10-14 20:26:34 +0530

    Add files via upload

---
+++ precache-manifest.5b6f4b1f808375c7e4c03f3b24ca9684.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "02a043977db67113b327",
+    "url": "/CricLive/static/css/main.f64c7ae4.chunk.css"
+  },
+  {
+    "revision": "02a043977db67113b327",
+    "url": "/CricLive/static/js/main.02a04397.chunk.js"
+  },
+  {
+    "revision": "766be002cfdbb69832ec",
+    "url": "/CricLive/static/js/1.766be002.chunk.js"
+  },
+  {
+    "revision": "b2a926c0856477e9c32f",
+    "url": "/CricLive/static/css/2.91403d14.chunk.css"
+  },
+  {
+    "revision": "b2a926c0856477e9c32f",
+    "url": "/CricLive/static/js/2.b2a926c0.chunk.js"
+  },
+  {
+    "revision": "50435d138be0bc0ab49f",
+    "url": "/CricLive/static/js/runtime~main.50435d13.js"
+  },
+  {
+    "revision": "d2b39f55a66d64245192383ca02613cf",
+    "url": "/CricLive/index.html"
+  }
+];
